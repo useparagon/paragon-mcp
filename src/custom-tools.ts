b641a3c1a7d673b68b85a0be6c26ac214d7b132b
jackmuva <jackmu@umich.edu>
2025-07-30 22:20:12 -0700

    have markdown parsing working for basic blocks

--- src/custom-tools.ts
+++ src/custom-tools.ts
@@ -1,6 +1,10 @@
 import { ExtendedTool } from "./type";
 import { envs } from "./utils";
 import { handleResponseErrors } from "./utils";
+// @ts-ignore
+import { markdown } from 'markdown';
+// @ts-ignore
+import { HTMLToJSON } from 'html-to-json-parser';
 
 export function getCustomTools(): Array<ExtendedTool> {
   const customTools: Array<ExtendedTool> = [];
@@ -42,11 +46,12 @@ export async function performCustomAction(
   }
 }
 
-export async function performCustomNotionCreate(
+async function performCustomNotionCreate(
   actionParams: any,
   jwt: string
 ) {
   console.log(`DEBUG:`, "Running custom notion action", actionParams);
+  const children = await markdownToJson(actionParams.content);
   try {
     const url = `${envs.ACTIONKIT_BASE_URL}/projects/${envs.PROJECT_ID}/actions`;
     const response = await fetch(url, {
@@ -60,22 +65,7 @@ export async function performCustomNotionCreate(
         parameters: {
           parent: { page_id: actionParams.parent },
           properties: { title: [{ text: { content: actionParams.title } }] },
-          children: [
-            {
-              object: "block",
-              type: "paragraph",
-              paragraph: {
-                rich_text: [
-                  {
-                    type: "text",
-                    text: {
-                      content: actionParams.content
-                    }
-                  }
-                ]
-              }
-            }
-          ]
+          children: children,
         }
       }),
     });
@@ -85,5 +75,87 @@ export async function performCustomNotionCreate(
   } catch (error) {
     throw error;
   }
+}
 
+async function markdownToJson(markdownString: string) {
+  const html = markdown.toHTML(markdownString);
+  const json = await HTMLToJSON(`<div> ${html} </div>`);
+  const result = [];
+  for (const block of json.content) {
+    const notionBlock = toNotionBlock(block);
+    if (notionBlock) {
+      result.push(notionBlock);
+    }
+  }
+  return result;
+}
+
+//NOTE: for full list of notion block types
+//https://developers.notion.com/reference/block#block-type-objects
+function toNotionBlock(block: any) {
+  let result = null;
+  if (typeof block === 'object') {
+    if (block.type === 'p') {
+      result = {
+        object: "block",
+        type: "paragraph",
+        paragraph: {
+          rich_text: [
+            {
+              type: "text",
+              text: {
+                content: block.content[0]
+              }
+            }
+          ]
+        }
+      };
+    } else if (block.type === 'h1') {
+      result = {
+        object: "block",
+        type: "heading_1",
+        heading_1: {
+          rich_text: [
+            {
+              type: "text",
+              text: {
+                content: block.content[0]
+              }
+            }
+          ]
+        }
+      };
+    } else if (block.type === 'h2') {
+      result = {
+        object: "block",
+        type: "heading_2",
+        heading_2: {
+          rich_text: [
+            {
+              type: "text",
+              text: {
+                content: block.content[0]
+              }
+            }
+          ]
+        }
+      };
+    } else if (block.type === 'h3') {
+      result = {
+        object: "block",
+        type: "heading_3",
+        heading_3: {
+          rich_text: [
+            {
+              type: "text",
+              text: {
+                content: block.content[0]
+              }
+            }
+          ]
+        }
+      };
+    }
+  }
+  return result;
 }
